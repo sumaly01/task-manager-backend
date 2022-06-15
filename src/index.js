0de72fc61a067b0aac559a7d57811779c3f8c5ec
sumaly01 <sumalybajra@gmail.com>
2022-06-15 23:09:00 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+//db lai access
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+const port = process.env.PORT;
+
+//parse json object to js object
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
